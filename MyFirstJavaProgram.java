6f853cffa4804bb8c10268ac5244b56a21f4829f
subbat9710 <teela.subba1991@gmail.com>
2017-07-14 11:36:15 -0400

    My First Java Learning Day

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,28 @@
+public class MyFirstJavaProgram {
+	 /* This is my first java program.
+    * This will print 'Hello World' as the output
+    * This is an example of multi-line comments.
+    */
+
+   public static void main(String []args) {
+      // This is an example of single line comment
+      /* This is also an example of single line comment. */
+      System.out.println("Hello World");
+   }
+}
+
+// Java tutorial.
+// 1. In order to check whether the java was installed in your mac. type java -version 
+//    looks like: Java (TM) SE Runtime Environment (build 1.8.0_20-b26)
+//                Java HotSpot (TM) 64-Bit Server VM (build 25.20-b23, mixed mode)
+// 2. Which java
+//   /user/bin/java
+
+//3. In order to run on the terminal.
+//   a. $ cd Java
+//   b. $ ls 
+//   c. $ Puppy.java
+//   d. $ javac Puppy.java
+//   e. $ java Puppy
+//   will show the out put in the terminal
+//   f. $ Hello World
